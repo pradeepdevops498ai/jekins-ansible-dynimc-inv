a7804e3499a64e395ca16ae28d5410f0525d50a4
pradeepdevops498ai <pradeepdevops498@gmail.com>
2024-11-21 21:26:06 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region = "ap-southeast-2"
 resource "aws_instance" "AWSINSTANCE"{
 ami = "ami-06d2149e11dd4bec4"
 instance_type = "t2.micro"
-key_name = "devopsaug2024"
+key_name = "Devops"
 security_groups = ["launch-wizard-3"]
 tags={
 Name = "terraformserver"
